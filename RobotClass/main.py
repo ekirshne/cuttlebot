3e16a324eb3ca50ee78cd22ad80059a081d2e2d3
Cuttlebot <cuttlebot@uci.edu>
2024-04-20 16:43:41 -0700

    Second batch of tests: Finished High Predator; Low prey and partially tested Low Predator; High Prey. Taking break to charge the RVR

--- RobotClass/main.py
+++ RobotClass/main.py
@@ -17,7 +17,7 @@ robot = Robot(
     
     #Define the number of predators and prey
     environment_type="HOMOGENEOUS", #{HOMOGENEOUS (Mixed), HETEROGENEOUS (Split), and PAIRED (Preditors & Prey close to eachother)} Change the Environment Type if you are changing the layout of the predators and prey
-    num_predators=3,
+    num_predators=1,
     num_prey=3,
 
     #define the amount of time (in minutes to run the robot)
